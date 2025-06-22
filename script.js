9df2c7f64d0d1785a1b5e404b0379e093b54a00f
Hitesh Rathi <97667730+HiteshRathi02@users.noreply.github.com>
2025-06-22 08:11:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-//your JS code here. If required.
+document.getElementById('voteForm').addEventListener('submit', function (e) {
+  e.preventDefault(); // prevent actual form submission
+
+  const age = document.getElementById('age').value.trim();
+  const name = document.getElementById('name').value.trim();
+
+  if (!age || !name) {
+    alert("Please enter valid details.");
+    return;
+  }
+
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (parseInt(age) > 18) {
+        resolve(name);
+      } else {
+        reject(name);
+      }
+    }, 4000);
+  })
+    .then((name) => {
+      alert(`Welcome, ${name}. You can vote.`);
+    })
+    .catch((name) => {
+      alert(`Oh sorry ${name}. You aren't old enough.`);
+    });
+});
